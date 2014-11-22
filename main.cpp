4d88d2032b32ecfd011f89738ac93cd8d249eba2
rotkreis <rotkreis@gmail.com>
2014-11-22 09:07:48 +0800

    First Commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EulerEquations
+//
+//  Created by Li Xinrui on 11/22/14.
+//  Copyright (c) 2014 Li Xinrui. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
