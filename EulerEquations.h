0d342184e9efab23b6acfd1b575fbb66c35acb04
rotkreis <rotkreis@gmail.com>
2014-11-23 12:26:32 +0800

    Working Smooth, yet unsastisfied about data structure

---
+++ EulerEquations.h
@@ -0,0 +1,126 @@
+//
+//  EulerEquations.h
+//  EulerEquations
+//
+//  Created by Li Xinrui on 11/23/14.
+//  Copyright (c) 2014 Li Xinrui. All rights reserved.
+//
+
+#ifndef __EulerEquations__EulerEquations__
+#define __EulerEquations__EulerEquations__
+
+#include <stdio.h>
+#include <cassert>
+#include "Matrix.h"
+typedef double (*pFunc)(double x);
+
+
+
+class Profiles : public Matrix{ // Mathematical Subscripts, index = 1, the first element
+public:
+    Profiles(int nCells):
+    Matrix(3, nCells) {}
+public:
+    double& u1(int index){
+        assert(index > 0);
+        return data[index - 1][0];
+    }
+    double& u2(int index){
+        return data[index - 1][1];
+    }
+    double& u3(int index){
+        return data[index - 1][2];
+    }
+};
+
+
+
+class EulerSolver{
+public:
+    int nCells;
+    double xStep;
+    double timeStep;
+    double gamma; // ratio of specific heat...
+    pFunc ivDensity;
+    pFunc ivPressure;
+    pFunc ivVelocity;
+    double startTime;
+    double finalTIme;
+    double xMin;
+    double xMax;
+    double CFI = 0.5;
+    
+    // Constructor
+public:
+    EulerSolver(pFunc rho, pFunc p, pFunc u):
+    ivDensity(rho), ivPressure(p),ivVelocity(u){}
+    
+    // Initiate
+public:
+    void SetRange(double x1, double x2){
+        xMin = x1;
+        xMax = x2;
+    }
+    void SetTime(double t1, double t2){
+        startTime = t1;
+        finalTIme = t2;
+    }
+    void SetGamma(double y){
+        gamma = y;
+    }
+    void SetCellNumber(int n){
+        nCells = n;
+    }
+    // Computations
+public:
+    void ComputeSpatialStep(){
+        xStep = (xMax - xMin) / nCells;
+    }
+    double IVAverage(int index, double x1, double x2, int n){
+        assert(x2 > x1);
+        double h = (x2 - x1) / (n - 1);
+        double sum = 0;
+        switch (index) {
+            case 1:
+                for (int i = 0; i <= n - 1; i++) {
+                    sum += ivDensity(x1 + i * h);
+                }
+                break;
+            case 2:
+                for (int i = 0; i <= n - 1; i++) {
+                    sum += ivDensity(x1 + i * h) * ivVelocity(x1 + i * h);
+                }
+                break;
+            case 3:
+                for (int i = 0; i <= n - 1; i++) {
+                    sum += 0.5 * ivDensity(x1 + i * h) * pow(ivVelocity(x1 + i * h), 2)
+                    + ivPressure(x1 + i * h) / (gamma - 1);
+                }
+            default:
+                break;
+        }
+        return sum / n;
+    }
+    void InitiateValues(Profiles& values){
+        for (int i = 1; i <= nCells; i++) {
+            values.u1(i) = IVAverage(1, xMin + (i - 1) * xStep, xMin + i * xStep, 10);
+            values.u2(i) = IVAverage(2, xMin + (i - 1) * xStep, xMin + i * xStep, 10);
+            values.u3(i) = IVAverage(3, xMin + (i - 1) * xStep, xMin + i * xStep, 10);
+        }
+    }
+    void test(){
+        ComputeSpatialStep();
+        Profiles values(nCells);
+        InitiateValues(values);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+};
+
+#endif /* defined(__EulerEquations__EulerEquations__) */
--- main.cpp
+++ main.cpp
@@ -8,8 +8,43 @@
 
 #include <iostream>
 #include "Matrix.h"
+#include "EulerEquations.h"
+// Primitive
+double rho(double x){
+    if (x < 0.5) {
+        return 1;
+    }
+    else {
+        return  0.125;
+    }
+}
+double p(double x){
+    if (x < 0.5) {
+        return 1;
+    }
+    else {
+        return 0.1;
+    }
+}
+double u(double x){
+    return 0;
+}
+
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::vector<mVector> nihao(3);
+    nihao[1] = mVector(3);
+    nihao[1][1] = 1.1;
+    std::cout << nihao[1] << endl;
+    
+    EulerSolver sol(rho, p, u);
+    sol.SetCellNumber(11);
+    sol.SetGamma(1.4);
+    sol.SetRange(0, 1);
+    sol.SetTime(0, 0.25);
+    sol.test();
+    
+    
     return 0;
 }
